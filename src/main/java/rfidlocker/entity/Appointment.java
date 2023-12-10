8942ee0f8b4bc0f8a2bc8d399b595a60fcb26543
liuchunlin@aol.com <liuchunlin@aol.com>
2023-12-10 16:18:58 +0800

    merge dev_kevin branch to develop barnch

--- src/main/java/rfidlocker/entity/Appointment.java
+++ src/main/java/rfidlocker/entity/Appointment.java
@@ -2,10 +2,14 @@ package rfidlocker.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
 public class Appointment {
 	
 	@Id
